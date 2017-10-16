4ad5ebdfae7db5b3df945eebbf70c92aa395494f
rajkripal <es12b1007@iith.ac.in>
2017-10-15 22:07:28 -0400

    Bug fixes in mcs openMP barrier

--- openMP/mcs.c
+++ openMP/mcs.c
@@ -21,40 +21,46 @@ void treeBarrier (bool sense) {
 
 	while (memcmp (nodes[threadID].childNotReady, 
 					(bool[]) {false, false, false, false}, 
-					sizeof (nodes[threadID].childNotReady)) != 0) {
-		for (int i = 0; i <  4; i++) {
-			nodes[threadID].childNotReady[i] = nodes[threadID].haveChild[i];
-		}
-		*nodes[threadID].parentPointer = false;
-		if (threadID != 0) {
-			while (nodes[threadID].parentSense != sense);
-		}
-
-		*nodes[threadID].childPointers[0] = sense;
-		*nodes[threadID].childPointers[1] = sense;
-
-		sense = !sense;
+					sizeof (nodes[threadID].childNotReady)) != 0);
+	for (int i = 0; i <  4; i++) {
+		nodes[threadID].childNotReady[i] = nodes[threadID].haveChild[i];
 	}
+	*nodes[threadID].parentPointer = false;
+	if (threadID != 0) {
+		while (nodes[threadID].parentSense != sense);
+	}
+
+	*nodes[threadID].childPointers[0] = sense;
+	*nodes[threadID].childPointers[1] = sense;
+
+	sense = !sense;
 }
 
 int main (int argc, char ** argv) {
 	
 	for (int i = 0; i < NUM_THREADS; i++) {
 		for (int j = 0; j <  4; j++) {
-			nodes[i].haveChild[j] = 4 * i + j < NUM_THREADS ? true : false;
+			nodes[i].haveChild[j] = 4 * i + j < NUM_THREADS - 1 ? true : false;
 			nodes[i].childNotReady[j] = nodes[i].haveChild[j];
 		}
 
 		nodes[i].parentPointer = i == 0 ? &nodes[i].dummy : &nodes[(i-1) / 4].childNotReady[(i-1) % 4];
-		nodes[i].childPointers[0] = 2 * i + 1 >= NUM_THREADS ? &nodes[i].dummy : &nodes[2 * i + 1].parentSense;
-		nodes[i].childPointers[1] = 2 * i + 2 >= NUM_THREADS ? &nodes[i].dummy : &nodes[2 * i + 2].parentSense;
+		nodes[i].childPointers[0] = 2 * i + 1 < NUM_THREADS ? &nodes[2 * i + 1].parentSense : &nodes[i].dummy;
+		nodes[i].childPointers[1] = 2 * i + 2 < NUM_THREADS ? &nodes[2 * i + 2].parentSense : &nodes[i].dummy;
 
 		nodes[i].parentSense = false;
+		nodes[i].dummy = false;
 	}
+
+	omp_set_num_threads(NUM_THREADS);
 
 	#pragma omp parallel
 	{
 		bool sense = true;
+		int ID = omp_get_thread_num();
+		printf ("Thread %d entering MCS Barrier\n", ID);
+		treeBarrier (sense);
+		printf ("Thread %d exiting MCS Barrier\n", ID);
 
 	}
 }
