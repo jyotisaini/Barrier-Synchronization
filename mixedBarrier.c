f112aea776edc7e4d3143398b1fb767b9a6eecb6
rajkripal <es12b1007@iith.ac.in>
2017-10-17 23:04:57 -0400

    removing debug messages

--- mixedBarrier.c
+++ mixedBarrier.c
@@ -7,7 +7,6 @@
 #include <math.h>
 #include <unistd.h>
 
-#define NUM_THREADS 5
 
 typedef struct treenode{
 	bool parentSense;
@@ -19,7 +18,6 @@ typedef struct treenode{
 } treenode;
 
 void treeBarrier (bool* sense, treenode * node) {
-	// printf("%c\n", node -> childNotReady[0]? 't' : 'f');
 	int threadID = omp_get_thread_num();
 	int i;
 
@@ -30,7 +28,7 @@ void treeBarrier (bool* sense, treenode * node) {
 	for (i = 0; i <  4; i++) {
 		node -> childNotReady[i] = node -> haveChild[i];
 	}
-	
+
 	*node -> parentPointer = false;
 	if (threadID != 0) {
 		while (node -> parentSense != *sense);
@@ -49,20 +47,15 @@ void disseminationBarrier (int rank, int numProcessors) {
 	MPI_Request sendRequest, recvRequest;
 	MPI_Status status;
 	while (round < (int)ceil(log(numProcessors * 1.0) / log(2.0))) {
-		// if(sendRequest) {
-		// 	MPI_Wait(&sendRequest, &status);
-		// }
 		int recepient = (rank - (int)pow (2, round)) % numProcessors;
 		if (recepient < 0) {
 			recepient = recepient + numProcessors;
 		}
 		
 		MPI_Isend(&sendBuf, 1, MPI_INT, (rank + (int)pow (2, round)) % numProcessors, 0, MPI_COMM_WORLD, &sendRequest);
-		printf ("sent message from %d to %d\n", rank, (rank + (int)pow (2, round)) % numProcessors);
 		MPI_Irecv(&recvBuf, 1, MPI_INT, recepient, 0, MPI_COMM_WORLD, &recvRequest);
 
 		MPI_Wait (&recvRequest, &status);
-		printf ("Received message sent from %d to %d\n", recepient, rank);
 		round ++;
 	}
 }
@@ -128,7 +121,7 @@ int main (int argc, char ** argv) {
 		sleep (10);
 	}
 
-	for (int i = 0; i < 2; i ++) {
+	for (int i = 0; i < num_barriers; i ++) {
 		printf ("Hello world from processor %d\n", worldRank);
 		printf ("Processor %d entering barrier %d\n", worldRank, i);
 		disseminationBarrier (worldRank, worldSize);
