04009edac8dfce7638f2c177e311f0fe0bfef03e
rajkripal <es12b1007@iith.ac.in>
2017-10-19 18:32:11 -0400

    adding time calculation for dissemination and mcs barrier

--- mpi/dissemination.c
+++ mpi/dissemination.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <mpi.h>
 #include <unistd.h>
-
+#include <sys/time.h>
 
 
 void disseminationBarrier (int rank, int numProcessors) {
@@ -27,8 +27,18 @@ void disseminationBarrier (int rank, int numProcessors) {
 	}
 }
 
-int main () {
+int main (int argc, char ** argv) {
 	MPI_Init (NULL, NULL);
+	int numBarriers;
+	struct timeval start, end;
+	double elapsedTime = 0.0;
+
+	if (argc == 2) {
+		numBarriers = atoi (argv[1]);
+	} else {
+		printf ("Syntax Error: Correct syntax is ./dissemination <num_barriers>\n");
+		return -1;
+	}
 
 	int worldSize;
 	MPI_Comm_size (MPI_COMM_WORLD, &worldSize);
@@ -36,17 +46,24 @@ int main () {
 	int worldRank;
 	MPI_Comm_rank (MPI_COMM_WORLD, &worldRank);
 
-	if (worldRank == 0) {
-		sleep (10);
-	}
+	// if (worldRank == 0) {
+	// 	sleep (10);
+	// }
 	int i;
-	for (i = 0; i < 2; i ++) {
+	for (i = 0; i < numBarriers; i ++) {
 		printf ("Hello world from processor %d\n", worldRank);
 		printf ("Processor %d entering barrier %d\n", worldRank, i);
+
+		gettimeofday (&start, NULL);
 		disseminationBarrier (worldRank, worldSize);
+		gettimeofday (&end, NULL);
+
 		printf ("Processor %d out of barrier %d\n", worldRank, i);
+
+		elapsedTime += (end.tv_sec - start.tv_sec) * 1000.0;
+		elapsedTime += (end.tv_usec - start.tv_usec) / 1000.0;
 	}
 
+	printf ("Average time spent by processor %d is %f\n", worldRank, elapsedTime / (numBarriers * 1.0));
 	MPI_Finalize();
-
 }
--- openMP/mcsBarrier.c
+++ openMP/mcsBarrier.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/time.h>
 #include "omp.h"
 
 
@@ -16,7 +17,6 @@ typedef struct treenode{
 
 
 void treeBarrier (bool* sense, treenode * node) {
-	// printf("%c\n", node -> childNotReady[0]? 't' : 'f');
 	int threadID = omp_get_thread_num();
 	int i;
 
@@ -43,10 +43,14 @@ void treeBarrier (bool* sense, treenode * node) {
 int main (int argc, char ** argv) {
 
 	int num_threads, num_barriers;
+	double timeTaken = 0.0;
 
 	if (argc == 3) {
 		num_threads = atoi (argv[1]);
 		num_barriers = atoi (argv[2]);
+	} else {
+		printf ("Syntax error: Corect syntax is ./mcsBarrier <num_processors> <num_barriers>\n");
+		return -1;
 	}
 	omp_set_num_threads(num_threads);
 	treenode * nodes[num_threads];
@@ -77,16 +81,30 @@ int main (int argc, char ** argv) {
 		*sense = true;
 		int ID = omp_get_thread_num();
 		int k;
+		struct timeval start, end;
+		double elapsedTime = 0.0;
 		for (k = 0; k < num_barriers; k++) {
 			printf ("Thread %d entering MCS Barrier %d\n", ID, k);
+			gettimeofday (&start, NULL);
 			treeBarrier (sense, nodes[ID]);
+			gettimeofday (&end, NULL);
 			printf ("Thread %d exiting MCS Barrier %d\n", ID, k);
+			elapsedTime += (end.tv_sec - start.tv_sec) * 1000.0;
+			elapsedTime += (end.tv_usec - start.tv_usec) / 1000.0;
 		}
 		
+		printf ("Average time spent by thread %d in MCS Barrier is %f\n", ID ,elapsedTime/(num_barriers * 1.0));
 		free (sense);
+		#pragma omp critical
+		{
+			timeTaken += elapsedTime;
+		}
 	}
 
 	for (i = 0; i < num_threads; i++) {
 		free (nodes[i]);
 	}
+
+	printf ("Average time spent by a thread in MCS Barrier is %f\n", timeTaken / (num_barriers * num_threads * 1.0));
+	return 0;
 }
