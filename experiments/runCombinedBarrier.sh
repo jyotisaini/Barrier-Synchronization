ee39ed186eca07f9017a5f38f68edb2fe3799eda
Jyoti Saini <jsaini7@jinx-login.cc.gatech.edu>
2017-10-20 22:18:48 -0400

    added run script for combined barrier

--- experiments/runCombinedBarrier.sh
+++ experiments/runCombinedBarrier.sh
@@ -1,12 +1,13 @@
 #PBS -q cs6210
 #PBS -l nodes=1:sixcore
-#PBS -l walltime=00:05:00
+#PBS -l walltime=00:20:00
 #PBS -N tournamentAndDissemination
 OMPI_MCA_mpi_yield_when_idle=0
-for processor in 2 4 6 8 10 12
+for processor in 2 4 8 10 12
 do
   for threads in 2 4 6 8
   do
-     mpirun --hostfile $PBS_NODEFILE -np $processor ./combinedBarrier $threads 5000 >> Thread-$threads-Proc-$processor.log
+     mpirun --hostfile $PBS_NODEFILE -np $processor ./mixedBarrier $threads 1000 1000  >> HeavyProcessThread$threads-Proc-$processor.log
+     mpirun --hostfile $PBS_NODEFILE -np $processor ./mixedBarrier $threads 1000 100  >>  LightProcessThread$threads-Proc-$processor.log
   done
 done
